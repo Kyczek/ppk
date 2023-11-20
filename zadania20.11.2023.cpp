4ab11bd9194afd8e4755a5cad55ce7800cd98ffe
Kyczek <146721554+Kyczek@users.noreply.github.com>
2023-11-20 12:55:31 +0100

    Add files via upload

---
+++ zadania20.11.2023.cpp
@@ -0,0 +1,38 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+
+struct tlumaczenie { 
+    std::string pl;
+    std::string en;
+};
+typedef std::vector<tlumaczenie> wektor;
+
+
+wektor load (const std::string& fileName)
+{
+    wektor w1;
+    std::ifstream in(fileName);
+    double x;
+    while(in >> x)
+    {
+        w1.push_back(x);
+        w1.push_back(x);
+    }
+    in.close();
+    return w1;
+}
+
+
+
+
+int main()
+{
+    
+}
+
